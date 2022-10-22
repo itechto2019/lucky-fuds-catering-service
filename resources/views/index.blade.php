7093a23260bf050c65e3676d680e378c5cfe1d7c
Keuriseutian15 <keuriseutian15@gmail.com>
2022-10-22 11:17:26 +0800

    Inventory => Rentals

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,7 +47,7 @@
                         </div>   
                         <div class="for-link {{ Route::currentRouteName() === 'inventory' ? 'active' : '' }}" id="inventory">
                             <a href="" onclick="openInventory(event)">
-                                Inventory
+                                Rentals
                                 <svg id="inventory_icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                 </svg>
