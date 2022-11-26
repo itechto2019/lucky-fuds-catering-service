b0687f13bd548a8bfa79eb9459508def9660a6ef
Keuriseutian15 <keuriseutian15@gmail.com>
2022-11-27 06:27:19 +0800

    Update 2022_11_26_213547_create_verifies_table.php

--- database/migrations/2022_11_26_213547_create_verifies_table.php
+++ database/migrations/2022_11_26_213547_create_verifies_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('verifies', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->text('token');
             $table->timestamps();
